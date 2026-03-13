faf32b11badeae9d33cb4a473d3ef6d9e2a2d424
Duncan Caplan <duncan.caplan@outlook.com>
2026-03-13 19:28:48 +0000

    feat: complete main.tf resources

--- terraform/modules/environments/dev/main.tf
+++ terraform/modules/environments/dev/main.tf
@@ -1,19 +1,94 @@
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr
-    enable_dns_support = true
-    enable_dns_hostnames = true
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "todo-vpc-${var.env}"
+  }
 }
 
 resource "aws_subnet" "public" {
-    count = length(var.public_subnets)
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.public_subnets[count.index]
-    availability_zone = var.availability_zones[count.index]
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "todo-public-${var.availability_zones[count.index]}-${var.env}"
+  }
 }
 
 resource "aws_subnet" "private" {
-    count = length(var.private_subnets)
-    vpc_id = aws_vpc.main.id
-    cidr_block = var.private_subnets[count.index]
-    availability_zone = var.availability_zones[count.index]
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "todo-private-${var.availability_zones[count.index]}-${var.env}"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "todo-igw-${var.env}"
+  }
+}
+
+resource "aws_eip" "elastic_ip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "todo-nat-eip-${var.env}"
+  }
+}
+
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.elastic_ip.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "todo-natgw-${var.env}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "todo-public-rt-${var.env}"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  tags = {
+    Name = "todo-private-rt-${var.env}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
--- terraform/modules/environments/dev/outputs.tf
+++ terraform/modules/environments/dev/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-
+    description = "The ID of the VPC"
+    value = aws_vpc.main.id
 }
 
 output "public_subnets_ids" {
-
+    description = "The ID of the public subnets"
+    value = aws_subnet.public[*].id
 }
 
 output "private_subnets_ids" {
-
+    description = "The ID of the private subnets"
+    value = aws_subnet.private[*].id
 }
